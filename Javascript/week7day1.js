5eb68dd2a689cd1752a923b43b4982afa6924676
Kadeisha <kreid93@gmail.com>
2020-09-21 21:33:17 -0400

    daily code war

---
+++ Javascript/week7day1.js
@@ -0,0 +1,17 @@
+/*Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+Notes:
+Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+Input >> Output Examples
+minValue ({1, 3, 1})  ==> return (13)
+Explanation:
+(13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+
+minValue({5, 7, 5, 9, 7})  ==> return (579)
+Explanation:
+(579) is the minimum number could be formed from {5, 7, 5, 9, 7} , Without duplications*/
+
+function minValue(values){
+  let arr = Array.from(new Set(values)) //set is a collection of unique items- elements can't be repeated
+  return parseInt(arr.sort().join('')) //go through integers, sort method, join method to being them together
+}
