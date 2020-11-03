a5ce9d7ca3939ba7426fb60d23b9bac741f7ac8a
Kadeisha <kreid93@gmail.com>
2020-11-03 12:13:32 -0500

    daily code war

---
+++ Javascript/indivWeek2Day2.js
@@ -0,0 +1,12 @@
+/*I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.*/
+
+function arrayPlusArray(arr1, arr2) {
+  var join = arr1.concat(arr2);
+
+  function getSum(a,b) {
+    return a+b;
+  }
+    return join.reduce(getSum);
+}
